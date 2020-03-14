fc3ff5b98364463a8bcdbc067c66952b4958edc1
Brayan Bernal <brayanbernal0710@gmail.com>
2020-03-14 15:26:32 -0400

    agregando archivos y clases nuevos para realizar impresion de reporte, programa ya almacena y anula la factura, falta imprimirla

---
+++ DocFac.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventario1
+{
+    class DocFac
+    {
+        public int Id { get; set; }
+        public int NroFactura { get; set; }
+        public int NroControl { get; set; }
+        public string Fecha { get; set; }
+        public double MontoBruto { get; set; }
+        public int PorceIva { get; set; }
+        public double Iva { get; set; }
+        public double Total { get; set; }
+        public char Estatus { get; set; }
+        public int ResponsableId { get; set; }
+        public int ClienteId { get; set; }
+        public int CedulaCliente { get; set; }
+        public string NombreCliente { get; set; }
+        public DetsFactura Detalles { get; set; }
+        public DocFac() { }
+
+        public DocFac(int id, int nroFactura, int nroControl, string fecha, double montoBruto, int porceIva, double iva, double total, char estatus, int responsableId, int clienteId, int cedulaCliente, string nombreCliente, DetsFactura detalles)
+        {
+            Id = id;
+            NroFactura = nroFactura;
+            NroControl = nroControl;
+            Fecha = fecha;
+            MontoBruto = montoBruto;
+            PorceIva = porceIva;
+            Iva = iva;
+            Total = total;
+            Estatus = estatus;
+            ResponsableId = responsableId;
+            ClienteId = clienteId;
+            CedulaCliente = cedulaCliente;
+            NombreCliente = nombreCliente;
+            Detalles = detalles;
+        }
+    }
+}
